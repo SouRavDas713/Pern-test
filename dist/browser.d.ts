25927c5bd39f2a0fcdb4de497218fbb7e3762a55
SouravDas713 <sdsourav713@gmail.com>
2025-12-10 21:41:44 +0600

    first

---
+++ dist/browser.d.ts
@@ -0,0 +1,15 @@
+import * as Prisma from './internal/prismaNamespaceBrowser.ts';
+export { Prisma };
+export * as $Enums from './enums.ts';
+export * from './enums.ts';
+/**
+ * Model User
+ *
+ */
+export type User = Prisma.UserModel;
+/**
+ * Model UserAddress
+ *
+ */
+export type UserAddress = Prisma.UserAddressModel;
+//# sourceMappingURL=browser.d.ts.map
---
+++ dist/models.d.ts
@@ -0,0 +1,4 @@
+export type * from './models/User.ts';
+export type * from './models/UserAddress.ts';
+export type * from './commonInputTypes.ts';
+//# sourceMappingURL=models.d.ts.map
